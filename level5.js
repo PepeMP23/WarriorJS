9326090d342d6931e3e3f820427e98eb410fe88a
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-04-22 18:29:44 -0600

    Level five solved

---
+++ level5.js
@@ -0,0 +1,27 @@
+class Player {
+    /**
+     * Plays a warrior turn.
+     *
+     * @param {Warrior} warrior The warrior.
+    */
+  
+    constructor() {
+        this.health = 0;
+    }
+
+    playTurn(warrior) {
+        // Cool code goes here
+        if(warrior.feel().isEmpty()){
+            if(warrior.health() < warrior.maxHealth() && warrior.health() >= this.health){
+                warrior.rest()
+            } else {
+                warrior.walk()
+            }
+        }
+        else {
+            warrior.attack()
+        }
+        this.health = warrior.health()
+    }
+}
+  
