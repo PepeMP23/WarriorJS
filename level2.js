52d306e5fa2fe52b355c83dc4071fd9cce4fec5f
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-04-22 18:06:04 -0600

    level one solved

---
+++ level2.js
@@ -0,0 +1,16 @@
+class Player {
+    /**
+     * Plays a warrior turn.
+     *
+     * @param {Warrior} warrior The warrior.
+     */
+    playTurn(warrior) {
+      // Cool code goes here
+      if(warrior.feel().isEmpty()){
+        warrior.walk();
+      }
+      else{
+        warrior.attack();
+      }
+    }
+  }
