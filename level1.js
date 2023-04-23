45ffcfb5adc9760a8acfa940b9401d29758507d2
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-04-22 18:08:42 -0600

    Level one solved

--- level1.js
+++ level1.js
@@ -1,11 +1,12 @@
 class Player {
-  /**
-   * Plays a warrior turn.
-   *
-   * @param {Warrior} warrior The warrior.
-   */
-  playTurn(warrior) {
-    // Cool code goes here
-    warrior.walk();
-  }
+    /**
+     * Plays a warrior turn.
+     *
+     * @param {Warrior} warrior The warrior.
+     */
+    playTurn(warrior) {
+      // Cool code goes here
+      warrior.walk();
+    }
 }
+  
