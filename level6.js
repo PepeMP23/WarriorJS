bbbfae9c78a95586484160b906728cfdc9b4024e
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-04-22 18:34:07 -0600

    Level six solved

---
+++ level6.js
@@ -0,0 +1,31 @@
+class Player {
+    /**
+     * Plays a warrior turn.
+     *
+     * @param {Warrior} warrior The warrior.
+     */
+
+    constructor() {
+        this.health = 0;
+    }
+  
+    playTurn(warrior) {
+        // Cool code goes here
+        if(warrior.feel().isEmpty()){
+            if(warrior.health() < this.health && this.health < 10){
+                warrior.walk('backward')
+            }
+            else if(warrior.health() < warrior.maxHealth() && this.health < 12){
+                warrior.rest()
+            }
+            else{
+                warrior.walk()
+            }
+        }
+        else {
+            warrior.attack()
+        }
+        this.health = warrior.health()
+    }
+}
+  
