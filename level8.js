551188cb589d4e601691b01ed8de4d964ccec2d5
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-04-27 18:52:49 -0600

    Advanced

--- level8.js
+++ level8.js
@@ -1,42 +1,47 @@
 class Player {
-    /**
-     * Plays a warrior turn.
-     *
-     * @param {Warrior} warrior The warrior.
-     */
-  
-    constructor() {
-      this.health = 0;
-      let turno = 0;
+  /**
+   * Plays a warrior turn.
+   *
+   * @param {Warrior} warrior The warrior.
+   */
+
+  constructor() {
+    this.health = 0;
+  }
+
+
+  playTurn(warrior) {
+    // Cool code goes here
+    if(this.health = 0){
+      warrior.walk();
     }
-  
-  
-    playTurn(warrior) {
-      // Cool code goes here
+    else{
       warrior.shoot();
-  
-      /*
-        if(warrior.feel().isWall()){
-          warrior.pivot();
-        }
-        else{
-          if(warrior.feel().isEmpty()){
-            if(warrior.health() < this.health && this.health < 10){
-              warrior.walk('backward')
-            }
-            else if(warrior.health() < warrior.maxHealth() && this.health < 10){
-              warrior.rest()
-            }
-            else{
-              warrior.walk()
-            }
-          }
-          else {
-            warrior.attack()
-          }
-        }
-    */
-      
-      this.health = warrior.health();
     }
-}  
+    
+    /*
+      if(warrior.feel().isWall()){
+        warrior.pivot();
+      }
+      else{
+        if(warrior.feel().isEmpty()){
+          if(warrior.health() < this.health && this.health < 10){
+            warrior.walk('backward')
+          }
+          else if(warrior.health() < warrior.maxHealth() && this.health < 10){
+            warrior.rest()
+          }
+          else{
+            warrior.walk()
+          }
+        }
+        else {
+          warrior.attack()
+        }
+      }
+    this.health = warrior.health();
+  */
+    
+    this.health = 2;
+  }
+}
